e01a471aef61668e26a5566a09687f4accd35984
mhg14 <mohamadhasanghamari@yahoo.com>
2023-06-16 18:59:37 +0330

    adding the functionality to cancel a booking

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -66,12 +66,12 @@ func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 
 	resp := AuthResponse{
 		User:  user,
-		Token: createTokenFromUser(user),
+		Token: CreateTokenFromUser(user),
 	}
 	return c.JSON(resp)
 }
 
-func createTokenFromUser(user *types.User) string {
+func CreateTokenFromUser(user *types.User) string {
 	now := time.Now()
 	expires := now.Add(time.Hour * 4).Unix()
 	claims := jwt.MapClaims{
